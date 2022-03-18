5603ad12affdfd62c40789f4fd87e8e6ac210a87
Naveena-7 <naveena.vitatech@gmail.com>
2022-03-18 19:06:10 +0530

    adding files to repository

---
+++ src/api/FacultySubjects/model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const FacultySubjectsSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female", "Others"]
+    },
+    subjects:{
+        type: String,
+        required: true
+    },
+    branch: {
+        type: String,
+        required: true,
+        enum: ["ECE", "CSE", "EEE", "MECH", "IT"]
+    }
+}, {
+    timestamps: true
+});
+
+const model = mongoose.model('FacultySubjects', FacultySubjectsSchema)
+
+export default model
