f822c12b480c2dbaa80c644aff25ffd045f3ba5b
Naveena-7 <naveena.vitatech@gmail.com>
2022-03-19 10:16:34 +0530

    adding fiels to repository

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import students from './students'
 import departments from './departments'
 import faculty from './faculty'
+import FacultySubjects from './FacultySubjects'
 
 
 const router = new Router()
@@ -10,5 +11,6 @@ const router = new Router()
 router.use('/students', students)
 router.use('/departments', departments)
 router.use('/faculty',faculty)
+router.use('/FacultySubjects',FacultySubjects)
 
 export default router
