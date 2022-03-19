b3725fdad15be94c0e2fd2b80115e903628659d9
Naveena-7 <101179594+Naveena-7@users.noreply.github.com>
2022-03-19 10:43:41 +0530

    Update index.js

--- src/api/faculty/index.js
+++ src/api/faculty/index.js
@@ -11,22 +11,22 @@ import {
 
 const router = new Router()
 
-//POST Request http://localhost:8080/students
+//POST Request http://localhost:8080/faculty
 router.post('/', create)
 
-//PUT Request http://localhost:8080/students/123
+//PUT Request http://localhost:8080/faculty/123
 router.put('/:id', update)
 
-//GET Request http://localhost:8080/students/search
+//GET Request http://localhost:8080/faculty/search
 router.get('/search', searchfaculty)
 
-//GET Request http://localhost:8080/students
+//GET Request http://localhost:8080/faculty
 router.get('/', index)
 
-//GET Request http://localhost:8080/students/123
+//GET Request http://localhost:8080/faculty/123
 router.get('/:id', show)
 
-//DELETE Request http://localhost:8080/students/123
+//DELETE Request http://localhost:8080/faculty/123
 router.delete('/:id', destroy)
 
 
